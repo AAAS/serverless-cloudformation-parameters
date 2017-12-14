9381f04d3d85c3896b006ccf4a1f381c46d3de60
fillerwriter <bmorrison@aaas.org>
2017-12-14 14:44:58 -0500

    Add plugin code

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      'aws:package:finalize:mergeCustomProviderResources': this.mergeCustomParameters.bind(this)
+    };
+  }
+
+  mergeCustomParameters() {
+    this.serverless.cli.log("Adding Cloudformation Parameters...");
+
+    let template = this.serverless.service.provider.compiledCloudFormationTemplate;
+
+    if (this.serverless.service.custom.parameters) {
+      template.Parameters = this.serverless.service.custom.parameters;
+    }
+  }
+}
+
+module.exports = ServerlessPlugin;
